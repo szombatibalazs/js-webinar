617b9b2a657d67d8b65a7d2cc8114a76943eee5f
Balázs Szombati <balazs_szombati@epam.com>
2023-01-24 15:52:48 +0100

    Adds solutions to plus/async exercises.

--- plus/async/loadJson.js
+++ plus/async/loadJson.js
@@ -1,7 +1,7 @@
 /*
  * Rewrite the following code to use async/await 
  */
-module.exports = function loadJson(url) {
+/*module.exports = function loadJson(url) {
     return fetch(url)
         .then(response => {
             if (response.status == 200) {
@@ -10,4 +10,14 @@ module.exports = function loadJson(url) {
                 throw new Error(response.status);
             }
         });
+};*/
+
+module.exports = async function loadJson(url) {
+
+    const response = await fetch(url);
+    if (response.status == 200) {       // 200 -> HTTP status code
+        //console.log(response.status);
+        return response.json();
+    }
+    throw new Error(response.status);
 };
--- plus/async/sleep.js
+++ plus/async/sleep.js
@@ -5,3 +5,15 @@
  * @returns {Promise} the promise which resolves when wait if over
  *                    or rejects if parameter is not correct
  */
+
+module.exports = async function waitFor(s) {
+    const countMs = s * 1000;
+    const maxMs = 10000;
+
+    if (typeof s !== 'number') {
+        throw new Error('Incorrect argument!');
+    }
+    return new Promise((resolve) => {
+        setTimeout(resolve, Math.min(countMs, maxMs));
+    });
+}
